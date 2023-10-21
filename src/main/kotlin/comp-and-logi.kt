a20a2069cb35995957342f2ca196b6591d4bc2e9
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-10-21 12:44:25 +0630

    comparison and logical operators test

---
+++ src/main/kotlin/comp-and-logi.kt
@@ -0,0 +1,17 @@
+fun main(){
+    println("2 > 1 is ${2 > 1}")
+    println("2 < 1 is ${2 < 1}")
+
+    println("1 >= 1 is ${1 >= 1}")
+    println("1 <= 1 is ${1 <= 1}")
+
+    println("1 == 1 is ${1 == 1}")
+    println("1 != 2 id ${1 != 2}")
+
+    println("!(2 > 1) is ${!(2 > 1)}")
+
+    // !false ==> Logical Not ( ! )
+    // &&
+    // ||
+    
+}
