9a45f422d9accda8f070481be7d107179cf048ce
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-10-21 15:14:05 +0630

    inheritance in kotlin testing

--- src/main/kotlin/classes-and-oop.kt
+++ src/main/kotlin/classes-and-oop.kt
@@ -7,7 +7,31 @@ fun main(){
     val user2 = User("zedekiah", 19) // instantition
     println("User name: ${user2.name}")
     println("User age : ${user2.age}")
+
+    val dog = Dog("Buddy")
+    val cat = Cat("Whisters")
+
+    dog.makeSound()
+    cat.makeSound()
 }
 
 class User(val name: String, val age: Int){
+}
+
+open class Animal(val name: String){
+    open fun makeSound(){
+        println("$name make a generic animal sound")
+    }
+}
+
+class Dog(name: String) : Animal(name){
+    override fun makeSound() {
+        println("$name barks like a dog.")
+    }
+}
+
+class Cat(name: String) : Animal(name){
+    override fun makeSound(){
+        println("$name meows like a cat")
+    }
 }
