0da899c52d1760d8ad0fa912ee608c9b3018c5c0
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-10-21 15:30:48 +0630

    init block, example of consturctor in Java (testing)

---
+++ src/main/kotlin/init-block.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val user1 = User3("   Hans    ", 30)
+
+    val user2 = User3("  Elfriede   ", 60)
+
+    val user3 = User3(age = 60)
+
+    val user4 = User3("     ", 20)
+
+}
+
+class User3(name: String = "No Name", var age: Int){
+    val name: String
+    init{
+        if(name.isBlank()){
+            this.name = "No Name"
+        }else{
+            this.name = name.trim()
+        }
+        println("New User created Name: ${this.name}, Age: $age")
+    }
+}
