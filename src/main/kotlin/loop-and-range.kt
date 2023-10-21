dd5666dd9218501dddd3da026dde12eca9ef3eda
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-10-21 10:50:31 +0630

    loop and ranges test

---
+++ src/main/kotlin/loop-and-range.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    val names: Array<String> = arrayOf("Jim", "John", "Jenny", "Jamie")
+
+    for(name:String in names) println(name)
+
+    for(i:Int in 1..10) println(i)
+
+    for(i:Int in 10 downTo 1) println(i)
+
+    for(i:Int in 1..10 step 2) println(i)
+
+    for(i:Int in 1.rangeTo(10).step(2)) println(i)
+
+    for(i:Long in 10L downTo 1) println(i)
+
+    for(i:Char in 'a'..'z' step 2) println(i)
+
+}
