8619f374afadfa06b890db74cf1b7cba8e77ee55
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-11-01 21:45:45 +0630

    practice class and object

--- src/main/kotlin/hamster.kt
+++ src/main/kotlin/hamster.kt
@@ -10,7 +10,42 @@ class Hamster{
 
 }
 
+class Cup{
+    var percentFull = 0
+    val max = 100
+    fun add(x: Int): Int{
+        percentFull += x
+        if(percentFull > max){
+            percentFull = max
+        }
+        return percentFull
+    }
+}
+
+class House{
+    var sofa: String = ""
+}
+
 fun main(){
     val hamster = Hamster()
     println(hamster.exerices())
+
+    val c1 = Cup()
+    c1.percentFull = 50
+    val c2 = Cup()
+    c2.percentFull = 100
+    println(c1.percentFull)
+    println(c2.percentFull)
+
+    c1.add(20)
+    println(c1.percentFull)
+    c1.add(50)
+    println(c1.percentFull)
+
+    // can't reassign the val to a new House:
+    val house = House()
+    house.sofa = "Simple sleeper sofa: $89.00"
+    println(house.sofa)
+    house.sofa = "New leather sofa: $3,099.00"
+    println(house.sofa)
 }
